01a8b18af668cc24c8eecbd4a2b1de1d2ed96a75
xander254 <alexkinyanjui356@gmail.com>
2024-11-05 15:46:35 +0300

    a c program that creates (5) zombie processes

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -19,8 +19,8 @@ int create_zombie(void)
 	else if (pid > 0)
 	{
 		printf("Zombie process created, PID: %d\n", pid);
-		return (pid);
 	}
+	return (pid);
 }
 
 /**
@@ -47,11 +47,10 @@ int infinite_while(void)
 int main(void)
 {
 	int i = 0;
-	int id = 0;
 
 	for (i = 0; i < 5; i++)
 	{
-		id = create_zombie();
+		create_zombie();
 	}
 	infinite_while();
 	return (0);

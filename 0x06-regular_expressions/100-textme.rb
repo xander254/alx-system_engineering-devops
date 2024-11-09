632cfc243abe210e0689d01061fd11f35c11bbf1
xander254 <alexkinyanjui356@gmail.com>
2024-11-09 03:05:09 +0300

    A scipt that should output: [SENDER],[RECEIVER],[FLAGS]
        The sender phone number or name (including country code if present)
        The receiver phone number or name (including country code if present)
        The flags that were used

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -4,4 +4,4 @@
 # The receiver phone number or name (including country code if
 # present)
 # The flags that were used
-puts ARGV[0].scan(/\[From:(.*?)\]\[to:(.*?)\]\[FLAGS:(.*?)\].join(",")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")

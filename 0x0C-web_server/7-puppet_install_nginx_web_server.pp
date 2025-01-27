36cebfebaae439bcde5c9ae75472e730a9569237
xander254 <alexkinyanjui356@gmail.com>
2025-01-27 04:22:12 +0300

    a pupet manifest to set up a new ubuntu server with nginx

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,24 @@
-#Set up a new ubuntu server
+# Set up a new Ubuntu server
 
 exec { 'update the system':
-    command => 'usr/bin/apt-get update',
+  command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-    ensure => 'installed',
-    require => Exec['update the system']
+  ensure  => 'installed',
+  require => Exec['update the system'],
 }
 
 file { '/var/www/html/index.html':
-    content => 'Hello World!'
+  content => 'Hello World!',
 }
 
 exec { 'redirect':
-    command => 'sed -i "241\        rewrite ^/redirect https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default'
-        provider => 'shell'
+  command  => 'sed -i "241\    rewrite ^/redirect https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
 }
 
-service { 'ngix':
-    ensure => running,
-    require => package['nginx']
+service { 'nginx':
+  ensure  => running,
+  require => package['nginx'],
 }

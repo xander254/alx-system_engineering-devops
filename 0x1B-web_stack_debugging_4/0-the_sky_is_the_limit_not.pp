9dcc3ecc3f65c41291a9eb682f380b1537bc0000
xander254 <alexkinyanjui356@gmail.com>
2025-03-07 05:28:26 +0300

    increase ulimit in nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,53 +1,11 @@
-class nginx_limits_fix {
+# Increase nginx default limit to 496
 
-  # Ensure the Nginx config file exists with proper file limits
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    content => template('nginx/nginx.conf.erb'),
-    mode    => '0644',
-    require => Package['nginx'],
-  }
-
-  # Set system-wide file limits for Nginx
-  file_line { 'set_nginx_nofile_limits':
-    path   => '/etc/security/limits.conf',
-    line   => 'nginx soft nofile 1048576',
-    match  => '^nginx.*nofile',
-    ensure => present,
-  }
-
-  file_line { 'set_nginx_hard_limits':
-    path   => '/etc/security/limits.conf',
-    line   => 'nginx hard nofile 1048576',
-    match  => '^nginx.*nofile',
-    ensure => present,
-  }
-
-  # Adjust SystemD service limits for Nginx
-  file { '/etc/systemd/system/nginx.service.d/override.conf':
-    ensure  => file,
-    content => "[Service]\nLimitNOFILE=1048576\n",
-    mode    => '0644',
-    require => Package['nginx'],
-  }
-
-  # Reload systemd and restart Nginx
-  exec { 'reload-systemd':
-    command     => '/bin/systemctl daemon-reexec',
-    refreshonly => true,
-    subscribe   => File['/etc/systemd/system/nginx.service.d/override.conf'],
-  }
-
-  service { 'nginx':
-    ensure     => running,
-    enable     => true,
-    hasrestart => true,
-    require    => File['/etc/nginx/nginx.conf'],
-    subscribe  => File['/etc/nginx/nginx.conf'],
-  }
-
+# Increase ULIMIT
+exec { 'increase-ulimit':
+    command => '/bin/sed -i \'s/ULIMIT="-n 15"/ULIMIT="-n 4096"/\' /etc/default/nginx',
 }
 
-# Apply the class
-include nginx_limits_fix
-
+# Restart nginx to update changes
+exec { 'restert-nginx':
+    command => '/usr/sbin/service nginx restart',
+}
